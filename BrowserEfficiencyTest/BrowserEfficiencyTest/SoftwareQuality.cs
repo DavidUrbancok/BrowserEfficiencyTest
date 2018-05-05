a389f3a7ebcde9aa447dce0a1297124b0a8ef356
Dávid Urbančok <urbancok.david@gmail.com>
2018-05-05 23:01:50 +0200

    Create basic browser efficiency test

---
+++ BrowserEfficiencyTest/BrowserEfficiencyTest/SoftwareQuality.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace BrowserEfficiencyTest
+{
+    internal class SoftwareQuality : Scenario
+    {
+        internal SoftwareQuality()
+        {
+            Name = "SoftwareQuality";
+            DefaultDuration = 10;
+        }
+
+        public override void Run(RemoteWebDriver driver, string browser, CredentialManager credentialManager, ResponsivenessTimer timer)
+        {
+            driver.Navigate().GoToUrl("https://www.muni.cz");
+
+            var searchBox = driver.FindElementById("search");
+            
+            searchBox.SendKeys("Software Quality" + Keys.Enter);
+
+            driver.FindElementsByClassName("gs-title").First().Click();
+        }
+    }
+}
